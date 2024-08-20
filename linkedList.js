50883016a5d5c3363256919f41c887001516c7c1
annisssimo <anyalar04@gmail.com>
2024-08-20 19:16:10 +0300

    feat: add toString and append methods

---
+++ linkedList.js
@@ -0,0 +1,40 @@
+import Node from './node.js';
+
+class LinkedList {
+  constructor() {
+    this.headNode = null;
+  }
+
+  toString() {
+    let currentNode = this.headNode;
+    let result = '';
+
+    while (currentNode !== null) {
+      result += `( ${currentNode.value} ) -> `;
+      currentNode = currentNode.nextNode;
+    }
+
+    return result + 'null';
+  }
+
+  //append(value) adds a new node containing value to the end of the list
+  append(value) {
+    const newNode = new Node(value);
+
+    if (!this.headNode) {
+      this.headNode = newNode;
+      return;
+    }
+
+    let lastNode = this.headNode;
+    while (lastNode !== null) {
+      if (lastNode.nextNode === null) {
+        lastNode.nextNode = newNode;
+        break;
+      }
+      lastNode = lastNode.nextNode;
+    }
+  }
+}
+
+export default LinkedList;
---
+++ node.js
@@ -0,0 +1,7 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.nextNode = null;
+  }
+}
+export default Node;
